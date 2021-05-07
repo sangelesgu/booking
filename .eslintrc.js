1d4cbee8bde2d19ece3703b604bd0695696bffc0
sangelesgu <s.angelesgu@gmail.com>
2021-05-07 19:56:29 +0200

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+const Rules = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error',
+};
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': Rules.OFF,
+  },
+};
